3da23ba79ef88e54c4fea227cb030322120f9139
Jeremy Rimpo <jeremy.rimpo@servermonkey.com>
2017-10-10 22:53:36 -0500

    Update for MO rebase

--- src/main.cpp
+++ src/main.cpp
@@ -16,153 +16,132 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Mod Organizer.  If not, see <http://www.gnu.org/licenses/>.
 */
+#include "helper/privileges.h"
 
-#include "privileges.h"
-
-#include <QtCore/QCoreApplication>
-#include <QSettings>
 #include <QDir>
 #include <QDirIterator>
+#include <QSettings>
+#include <QtCore/QCoreApplication>
+#include <common/sane_windows.h>
+#include <common/stringutils.h>
 
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
+#pragma comment(linker, "/manifestdependency:\"name='dlls' version='1.0.0.0' type='win32'\"")
 
+std::wstring ToWString(const QString& source) {
+    wchar_t* buffer = new wchar_t[source.count() + 1];
+    source.toWCharArray(buffer);
+    buffer[source.count()] = L'\0';
+    std::wstring result(buffer);
+    delete[] buffer;
 
-#pragma comment(linker, "/manifestDependency:\"name='dlls' processorArchitecture='x86' version='1.0.0.0' type='win32' \"")
-
-
-std::wstring ToWString(const QString &source)
-{
-  wchar_t *buffer = new wchar_t[source.count() + 1];
-  source.toWCharArray(buffer);
-  buffer[source.count()] = L'\0';
-  std::wstring result(buffer);
-  delete [] buffer;
-
-  return result;
+    return result;
 }
 
-QString ToQString(const std::wstring &source)
-{
-#if QT_VERSION >= QT_VERSION_CHECK(5,0,0)
-  return QString::fromWCharArray(source.c_str());
-#else
-  return QString::fromUtf16(source.c_str());
-#endif
+QString ToQString(const std::wstring& source) { return QString::fromWCharArray(source.c_str()); }
+
+static bool createMODirectory(const QString& directoryName, const std::wstring& accountName) {
+    if (!QDir(directoryName).exists()) {
+        if (!QDir().mkdir(directoryName)) {
+            qCritical("Failed to create \"%s\", do you have the necessary access rights to the installation folder?",
+                      directoryName.toUtf8().constData());
+            return false;
+        }
+    }
+
+    if (!QFileInfo(directoryName).isDir()) {
+        qCritical("\"%s\" seems to be a regular file", directoryName.toUtf8().constData());
+        return false;
+    }
+
+    if (!SetOwner(ToWString(directoryName).c_str(), accountName.c_str())) {
+        qCritical("failed to set owner of \"%s\" to \"%ls\"", directoryName.toUtf8().constData(), accountName.c_str());
+        return false;
+    }
+
+    return true;
 }
 
-
-static bool createMODirectory(const QString &directoryName, const std::wstring &accountName)
-{
-  if (!QDir(directoryName).exists()) {
-    if (!QDir().mkdir(directoryName)) {
-      qCritical("Failed to create \"%s\", do you have the necessary access rights to the installation folder?", directoryName.toUtf8().constData());
-      return false;
+static bool init(const QString& mopath, const std::wstring& accountName) {
+    if (!SetOwner(ToWString(mopath).c_str(), accountName.c_str())) {
+        qCritical("failed to set owner of \"%s\" to \"%ls\"", mopath.toUtf8().constData(), accountName.c_str());
+        return false;
     }
-  }
 
-  if (!QFileInfo(directoryName).isDir()) {
-    qCritical("\"%s\" seems to be a regular file", directoryName.toUtf8().constData());
-    return false;
-  }
-
-  if (!SetOwner(ToWString(directoryName).c_str(), accountName.c_str())) {
-    qCritical("failed to set owner of \"%s\" to \"%ls\"", directoryName.toUtf8().constData(), accountName.c_str());
-    return false;
-  }
-
-  return true;
+    if (!createMODirectory(mopath.mid(0).append("\\profiles"), accountName) ||
+        !createMODirectory(mopath.mid(0).append("\\mods"), accountName) ||
+        !createMODirectory(mopath.mid(0).append("\\downloads"), accountName)) {
+        return false;
+    }
+    return true;
 }
 
+static bool backdateBSAs(const QString& dataPath) {
+    QStringList bsaFilter("*.bsa");
+    QDirIterator iter(dataPath, bsaFilter, QDir::Files);
+    while (iter.hasNext()) {
+        iter.next();
 
-static bool init(const QString &mopath, const std::wstring &accountName)
-{
-  if (!SetOwner(ToWString(mopath).c_str(), accountName.c_str())) {
-    qCritical("failed to set owner of \"%s\" to \"%ls\"",
-              mopath.toUtf8().constData(), accountName.c_str());
-    return false;
-  }
+        HANDLE file = ::CreateFileW(ToWString(iter.filePath()).c_str(), GENERIC_READ | GENERIC_WRITE, 0, nullptr,
+                                    OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+        if (file == INVALID_HANDLE_VALUE) {
+            qCritical("failed to open %s: errorcode %d", iter.filePath().toUtf8().constData(), ::GetLastError());
+            return false;
+        }
 
-  if (!createMODirectory(mopath.mid(0).append("\\profiles"), accountName) ||
-      !createMODirectory(mopath.mid(0).append("\\mods"), accountName) ||
-      !createMODirectory(mopath.mid(0).append("\\downloads"), accountName)) {
-    return false;
-  }
-  return true;
+        ULONGLONG temp = 0;
+        temp = (145731ULL) * 24 * 60 * 60 * 10000000ULL;
+
+        FILETIME newWriteTime;
+
+        newWriteTime.dwLowDateTime = (DWORD)(temp & 0xFFFFFFFF);
+        newWriteTime.dwHighDateTime = (DWORD)(temp >> 32);
+
+        if (!::SetFileTime(file, nullptr, nullptr, &newWriteTime)) {
+            qCritical("failed to change date for %s: errorcode %d", iter.filePath().toUtf8().constData(),
+                      ::GetLastError());
+            return false;
+        }
+
+        CloseHandle(file);
+    }
+    return true;
 }
 
-
-static bool backdateBSAs(const QString &dataPath)
-{
-  QStringList bsaFilter("*.bsa");
-  QDirIterator iter(dataPath, bsaFilter, QDir::Files);
-  while (iter.hasNext()) {
-    iter.next();
-
-    HANDLE file = ::CreateFileW(ToWString(iter.filePath()).c_str(), GENERIC_READ | GENERIC_WRITE,
-                               0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
-    if (file == INVALID_HANDLE_VALUE) {
-      qCritical("failed to open %s: errorcode %d", iter.filePath().toUtf8().constData(), ::GetLastError());
-      return false;
+int mainDelegate(int argc, wchar_t** argv) {
+    if (argc < 2) {
+        qCritical("Invalid number of parameters");
+        return -1;
     }
 
-    ULONGLONG temp = 0;
-    temp = (145731ULL) * 24 * 60 * 60 * 10000000ULL;
+    qDebug("action: %ls", argv[1]);
 
-    FILETIME newWriteTime;
-
-    newWriteTime.dwLowDateTime  = (DWORD)(temp & 0xFFFFFFFF);
-    newWriteTime.dwHighDateTime = (DWORD)(temp >> 32);
-
-    if (!::SetFileTime(file, nullptr, nullptr, &newWriteTime)) {
-      qCritical("failed to change date for %s: errorcode %d", iter.filePath().toUtf8().constData(), ::GetLastError());
-      return false;
+    if (wcscmp(argv[1], L"init") == 0) {
+        if (argc < 4) {
+            qCritical("Invalid number of parameters");
+            return -1;
+        }
+        qDebug("init: %ls - %ls", argv[2], argv[3]);
+        // set up mod organizer directory
+        if (!init(ToQString(argv[2]), argv[3])) {
+            return -2;
+        }
+    } else if (wcscmp(argv[1], L"backdateBSA") == 0) {
+        qDebug("backdate bsas in %ls", argv[2]);
+        if (!backdateBSAs(ToQString(argv[2]))) {
+            return -2;
+        }
+    } else {
+        return -1;
     }
 
-    CloseHandle(file);
-  }
-  return true;
+    return 0;
 }
 
-
-int mainDelegate(int argc, wchar_t **argv)
-{
-  if (argc < 2) {
-    qCritical("Invalid number of parameters");
-    return -1;
-  }
-
-  qDebug("action: %ls", argv[1]);
-
-  if (wcscmp(argv[1], L"init") == 0) {
-    if (argc < 4) {
-      qCritical("Invalid number of parameters");
-      return -1;
+int wmain(int argc, wchar_t** argv) {
+    int res = mainDelegate(argc, argv);
+    if (res != 0) {
+        qDebug("%d", res);
+        getchar();
     }
-    qDebug("init: %ls - %ls", argv[2], argv[3]);
-    // set up mod organizer directory
-    if (!init(ToQString(argv[2]), argv[3])) {
-      return -2;
-    }
-  } else if (wcscmp(argv[1], L"backdateBSA") == 0) {
-    qDebug("backdate bsas in %ls", argv[2]);
-    if (!backdateBSAs(ToQString(argv[2]))) {
-      return -2;
-    }
-  } else {
-    return -1;
-  }
-
-  return 0;
-}
-
-
-int wmain(int argc, wchar_t **argv)
-{
-  int res = mainDelegate(argc, argv);
-  if (res != 0) {
-    qDebug("%d", res);
-    getchar();
-  }
-  return res;
+    return res;
 }
--- src/privileges.cpp
+++ src/privileges.cpp
@@ -1,112 +1,104 @@
-#include "privileges.h"
+#include "helper/privileges.h"
+
 #include <QDebug>
-#include <stdio.h>
+
+#include <LMCons.h>
 #include <accctrl.h>
 #include <aclapi.h>
 #include <memory>
-#include <LMCons.h>
+#include <stdio.h>
 
+BOOL SetPrivilege(HANDLE token, LPCWSTR privilege, BOOL enable) {
+    TOKEN_PRIVILEGES tokenPriv;
+    LUID luid;
 
-BOOL SetPrivilege(HANDLE token, LPCWSTR privilege, BOOL enable)
-{
-  TOKEN_PRIVILEGES tokenPriv;
-  LUID luid;
+    // get the local id of the privilege
+    if (!LookupPrivilegeValue(nullptr, privilege, &luid)) {
+        qCritical("failed to look up privilege %ls: %lu", privilege, ::GetLastError());
+        return FALSE;
+    }
 
-  // get the local id of the privilege
-  if (!LookupPrivilegeValue(nullptr, privilege, &luid)) {
-    qCritical("failed to look up privilege %ls: %lu",
-              privilege, ::GetLastError());
-    return FALSE;
-  }
+    // set the privilege to either enabled or disabled
+    tokenPriv.PrivilegeCount = 1;
+    tokenPriv.Privileges[0].Luid = luid;
+    if (enable) {
+        tokenPriv.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+    } else {
+        tokenPriv.Privileges[0].Attributes = 0;
+    }
 
-  // set the privilege to either enabled or disabled
-  tokenPriv.PrivilegeCount = 1;
-  tokenPriv.Privileges[0].Luid = luid;
-  if (enable) {
-    tokenPriv.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
-  } else {
-    tokenPriv.Privileges[0].Attributes = 0;
-  }
+    // change the privilege
+    if (!AdjustTokenPrivileges(token, FALSE, &tokenPriv, sizeof(TOKEN_PRIVILEGES), (PTOKEN_PRIVILEGES) nullptr,
+                               (PDWORD) nullptr)) {
+        qCritical("failed to adjust privilege %ls: %lu", privilege, ::GetLastError());
+        return FALSE;
+    }
 
-  // change the privilege
-  if (!AdjustTokenPrivileges(token, FALSE, &tokenPriv,
-         sizeof(TOKEN_PRIVILEGES), (PTOKEN_PRIVILEGES)nullptr,
-         (PDWORD)nullptr)) {
-    qCritical("failed to adjust privilege %ls: %lu",
-              privilege, ::GetLastError());
-    return FALSE;
-  }
+    if (::GetLastError() == ERROR_NOT_ALL_ASSIGNED) {
+        qCritical("The token does not have the specified privilege %s.", privilege);
+        return FALSE;
+    }
 
-  if (::GetLastError() == ERROR_NOT_ALL_ASSIGNED) {
-    qCritical("The token does not have the specified privilege %s.",
-              privilege);
-    return FALSE;
-  }
-
-  return TRUE;
+    return TRUE;
 }
 
+BOOL SetOwner(LPCTSTR filename, LPCTSTR newOwner) {
+    PSID sid = nullptr;
+    BOOL res = TRUE;
+    PACL pacl = nullptr;
 
-BOOL SetOwner(LPCTSTR filename, LPCTSTR newOwner)
-{
-  PSID sid = nullptr;
-  BOOL res = TRUE;
-  PACL pacl = nullptr;
+    // get the SID for the new owner
+    TCHAR domainUnused[4096];
+    DWORD sidSize = 0;
+    DWORD domainBufSize = 4096;
+    SID_NAME_USE sidUse;
+    // pre-flight to determine required size of the sid
+    LookupAccountName(nullptr, newOwner, nullptr, &sidSize, domainUnused, &domainBufSize, &sidUse);
+    sid = (PSID)malloc(sidSize);
+    // determine sid for account name
+    if (!LookupAccountName(nullptr, newOwner, sid, &sidSize, domainUnused, &domainBufSize, &sidUse)) {
+        qCritical("failed to look up account name: %ls", newOwner);
+        res = FALSE;
+    } else {
+        EXPLICIT_ACCESS access;
+        ZeroMemory(&access, sizeof(EXPLICIT_ACCESS));
 
-  // get the SID for the new owner
-  TCHAR domainUnused[4096];
-  DWORD sidSize = 0;
-  DWORD domainBufSize = 4096;
-  SID_NAME_USE sidUse;
-  // pre-flight to determine required size of the sid
-  LookupAccountName(nullptr, newOwner, nullptr, &sidSize, domainUnused, &domainBufSize, &sidUse);
-  sid = (PSID)malloc(sidSize);
-  // determine sid for account name
-  if (!LookupAccountName(nullptr, newOwner, sid, &sidSize, domainUnused, &domainBufSize, &sidUse)) {
-    qCritical("failed to look up account name: %ls", newOwner);
-    res = FALSE;
-  } else {
-    EXPLICIT_ACCESS access;
-    ZeroMemory(&access, sizeof(EXPLICIT_ACCESS));
+        wchar_t ownerTemp[UNLEN + 1];
+        wcsncpy(ownerTemp, newOwner, UNLEN + 1);
 
-    wchar_t ownerTemp[UNLEN + 1];
-    wcsncpy(ownerTemp, newOwner, UNLEN + 1);
+        // Set full control for Administrators.
+        access.grfAccessPermissions = GENERIC_ALL;
+        access.grfAccessMode = SET_ACCESS;
+        access.grfInheritance = SUB_CONTAINERS_AND_OBJECTS_INHERIT;
+        access.Trustee.TrusteeForm = TRUSTEE_IS_SID;
+        access.Trustee.TrusteeType = TRUSTEE_IS_GROUP;
+        access.Trustee.ptstrName = (LPTSTR)sid;
 
-    // Set full control for Administrators.
-    access.grfAccessPermissions = GENERIC_ALL;
-    access.grfAccessMode = SET_ACCESS;
-    access.grfInheritance = SUB_CONTAINERS_AND_OBJECTS_INHERIT;
-    access.Trustee.TrusteeForm = TRUSTEE_IS_SID;
-    access.Trustee.TrusteeType = TRUSTEE_IS_GROUP;
-    access.Trustee.ptstrName = (LPTSTR)sid;
+        DWORD secRes = SetEntriesInAcl(1, &access, nullptr, &pacl);
+        if (secRes != ERROR_SUCCESS) {
+            qCritical("failed to set up acls: %lu", secRes);
+            return FALSE;
+        }
 
-    DWORD secRes = SetEntriesInAcl(1, &access, nullptr, &pacl);
-    if (secRes != ERROR_SUCCESS) {
-      qCritical("failed to set up acls: %lu", secRes);
-      return FALSE;
+        // filename parameter for SetNamedSecurityInfo isn't const
+        // which is odd since it is documented to be a input parameter...
+        TCHAR* fileNameBuf = new TCHAR[32768];
+        wcsncpy_s(fileNameBuf, 32768, filename, 32768);
+        // Set the owner on the file and give him full access
+        secRes =
+            SetNamedSecurityInfo(fileNameBuf, SE_FILE_OBJECT, OWNER_SECURITY_INFORMATION | DACL_SECURITY_INFORMATION,
+                                 sid, nullptr, pacl, nullptr);
+
+        delete[] fileNameBuf;
+        if (secRes != NOERROR) {
+            qCritical("failed to set file owner: %d", secRes);
+            res = false;
+        }
     }
 
-
-    // filename parameter for SetNamedSecurityInfo isn't const
-    // which is odd since it is documented to be a input parameter...
-    TCHAR *fileNameBuf = new TCHAR[32768];
-    wcsncpy_s(fileNameBuf, 32768, filename, 32768);
-    // Set the owner on the file and give him full access
-    secRes = SetNamedSecurityInfo(
-        fileNameBuf, SE_FILE_OBJECT,
-        OWNER_SECURITY_INFORMATION | DACL_SECURITY_INFORMATION,
-        sid, nullptr, pacl, nullptr);
-
-    delete [] fileNameBuf;
-    if (secRes != NOERROR) {
-      qCritical("failed to set file owner: %d", secRes);
-      res = false;
+    if (sid != nullptr) {
+        free(sid);
     }
-  }
 
-  if (sid != nullptr) {
-    free(sid);
-  }
-
-  return res;
+    return res;
 }
